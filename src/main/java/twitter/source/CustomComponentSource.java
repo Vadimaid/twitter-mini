331dd1c51b19d59899a380d501e5e5020504aa22
vadim <vadimaid@gmail.com>
2025-08-13 21:25:05 +0600

    Added password encoder

--- src/main/java/twitter/source/CustomComponentSource.java
+++ src/main/java/twitter/source/CustomComponentSource.java
@@ -3,6 +3,8 @@ package twitter.source;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.flywaydb.core.Flyway;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import twitter.configuration.ComponentMethod;
 import twitter.configuration.ComponentSource;
 import twitter.configuration.Value;
@@ -54,6 +56,11 @@ public class CustomComponentSource {
         allowedEndpoints.addEndpoint("/api/register");
 
         return allowedEndpoints;
+    }
+
+    @ComponentMethod
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(4);
     }
 
 }
