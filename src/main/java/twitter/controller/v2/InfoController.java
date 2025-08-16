474ad0a7c2529879ffb3dbf232feedffbb2022b4
Eldiar <eldik0196@gmail.com>
2025-08-16 17:48:47 +0600

    Добавил метод infoAll в интерфейс InfoController и реализовал в имплиментации.

--- src/main/java/twitter/controller/v2/InfoController.java
+++ src/main/java/twitter/controller/v2/InfoController.java
@@ -2,8 +2,12 @@ package twitter.controller.v2;
 
 import twitter.dto.v2.response.InfoResponseDto;
 
+import java.util.List;
+
 public interface InfoController {
 
     InfoResponseDto info(String username);
+
+    List<InfoResponseDto> infoAll();
 
 }
